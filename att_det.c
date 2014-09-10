5471df7cb5e3df7a59dd94a8658c44e4999528f8
nrainvil <nira6794@colorado.edu>
2014-09-10 12:46:23 -0600

    Eclipse stub

--- att_det.c
+++ att_det.c
@@ -1,9 +1,9 @@
+#include "att_det.h"
 #include <stdio.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_linalg.h>
 
-void est_sun_vec_ls(gsl_matrix* sun_sens_volt_mtrx_p, gsl_matrix* sun_sens_norm_mtrx_p, gsl_matrix* sun_vec_mtrx_p){
 /*
  * Function: est_sun_vec_ls
  * ------------------------
@@ -11,8 +11,10 @@ void est_sun_vec_ls(gsl_matrix* sun_sens_volt_mtrx_p, gsl_matrix* sun_sens_norm_
  *  based on sun sensor readings using
  *  a Least Squares algorithm
  *
- *  returns: Sun vector in the body frame
+ *  result: Sun vector in the body frame
  */
+
+int est_sun_vec_ls(gsl_matrix* sun_sens_volt_mtrx_p, gsl_matrix* sun_sens_norm_mtrx_p, gsl_matrix* sun_vec_mtrx_p){
 
 	/* Initialize Matrices */
 	int sun_signum;
@@ -20,6 +22,21 @@ void est_sun_vec_ls(gsl_matrix* sun_sens_volt_mtrx_p, gsl_matrix* sun_sens_norm_
 	gsl_matrix* sun_right_mtrx_p = gsl_matrix_alloc(3,1);
 	gsl_matrix* sun_left_mtrx_p = gsl_matrix_alloc(3,3);
 	gsl_matrix* sun_left_inverse_mtrx_p = gsl_matrix_alloc(3,3);
+
+	int i;
+	int ss_count = 0;
+	for (i=0;i<9;i++){
+		if(sun_sens_volt_mtrx_p->data[i]>SS_V_CUTOFF){
+			ss_count++;
+		}
+	}
+
+	if (ss_count < 3) { 
+		for (i=0;i<3;i++){
+			sun_vec_mtrx_p->data[i] = 0;
+		}
+		return 1;
+	}
 
 	/*Calculate Least Squares solution - inv(norm'*norm)*norm'*sun_sens*/
 	gsl_blas_dgemm(CblasTrans, CblasNoTrans, 1.0, sun_sens_norm_mtrx_p, sun_sens_volt_mtrx_p, 0.0, sun_right_mtrx_p);
@@ -32,31 +49,24 @@ void est_sun_vec_ls(gsl_matrix* sun_sens_volt_mtrx_p, gsl_matrix* sun_sens_norm_
 	gsl_matrix_free(sun_right_mtrx_p);
 	gsl_matrix_free(sun_left_mtrx_p);
 	gsl_matrix_free(sun_left_inverse_mtrx_p);
-	return;
+	return 0;
 }
 
-void est_svd(double* gyro_body_rates, double* mag_vec, double* sun_sens_volt, double* sun_sens_norm, double* sun_eci_vec, double* mag_eci_vec, double* att_quaternion, double* sat_body_rates){
 /*
- * Function: est_svd
+ * Function: est_svd_2v
  * -----------------
  *  Calculates an attitude estimate based on
- *  the SVD solution to Whaba's problem.
- *  
- *  The body frame sun vector is calculated 
- *  independantly by a Least Squares estimator
+ *  the SVD solution to Whaba's problem using
+ *  2 Body Vectors and 2 ECI vectors.
  *
- *  returns: ECI->Body Quaternion
- *           Body Rates
+ *  result: updated est_state 
  */
-
-	/*Constants */
-	int const sun_sens_len = 15;
+void est_svd_2v(gsl_matrix* body_1_vec_mtrx_p,gsl_matrix* body_2_vec_mtrx_p,gsl_matrix* eci_1_vec_mtrx_p,gsl_matrix* eci_2_vec_mtrx_p, struct est_state* state){
 
 	/*Initialize Matrices*/
 	int est_signum;
 	gsl_matrix* b_mtrx_p = gsl_matrix_alloc(3,3);
 	gsl_matrix_set_all(b_mtrx_p,0);
-	gsl_matrix* sun_vec_mtrx_p = gsl_matrix_alloc(3,1);
 	gsl_matrix* u_lu_mtrx_p = gsl_matrix_alloc(3,3);
 	gsl_matrix* v_mtrx_p = gsl_matrix_alloc(3,3);
 	gsl_matrix* v_lu_mtrx_p = gsl_matrix_alloc(3,3);
@@ -65,23 +75,10 @@ void est_svd(double* gyro_body_rates, double* mag_vec, double* sun_sens_volt, do
 	gsl_vector* work_vec_p = gsl_vector_alloc(3);
 	gsl_permutation* est_perm = gsl_permutation_alloc(3);
 
-	/*Vector to Matrix Conversion*/
-	gsl_matrix_view mag_vec_mtrx = gsl_matrix_view_array(mag_vec,3,1);
-	gsl_matrix_view sun_eci_vec_mtrx = gsl_matrix_view_array(sun_eci_vec,3,1);
-	gsl_matrix_view mag_eci_vec_mtrx = gsl_matrix_view_array(mag_eci_vec,3,1);
-	gsl_matrix_view sun_sens_volt_mtrx = gsl_matrix_view_array(sun_sens_volt,sun_sens_len,1);
-	gsl_matrix_view sun_sens_norm_mtrx = gsl_matrix_view_array(sun_sens_norm,sun_sens_len,3);
-
-	/*Calc Sun Vector*/ 
-	est_sun_vec_ls(&sun_sens_volt_mtrx.matrix, &sun_sens_norm_mtrx.matrix, sun_vec_mtrx_p);
-
-	printf("Sun Vector:\n");
-	gsl_matrix_fprintf(stdout, sun_vec_mtrx_p,"%.2f");
-
 	/*Start SVD Wahba Solution 
           Calc B = B + eci'*body*/
-	gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, &mag_eci_vec_mtrx.matrix, &mag_vec_mtrx.matrix, 1.0, b_mtrx_p);
-	gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, &sun_eci_vec_mtrx.matrix, sun_vec_mtrx_p, 1.0, b_mtrx_p);
+	gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, body_1_vec_mtrx_p, eci_1_vec_mtrx_p, 1.0, b_mtrx_p);
+	gsl_blas_dgemm(CblasNoTrans, CblasTrans, 1.0, body_2_vec_mtrx_p, eci_2_vec_mtrx_p, 1.0, b_mtrx_p);
 
 	/*Calc SVD*/
 	gsl_linalg_SV_decomp(b_mtrx_p, v_mtrx_p, s_vec_p, work_vec_p);
@@ -112,23 +109,76 @@ void est_svd(double* gyro_body_rates, double* mag_vec, double* sun_sens_volt, do
 	/*Convert DCM to Quaternion*/
 
 	/*Assign Output - TEMP*/
-	att_quaternion[0] = 1.1;
-	att_quaternion[1] = 1.2;
-	att_quaternion[2] = 3.1;
-	att_quaternion[3] = 1.4;
-	sat_body_rates[0] = gyro_body_rates[0];
-	sat_body_rates[1] = gyro_body_rates[1];
-	sat_body_rates[2] = gyro_body_rates[2];
+	state->att_quaternion[0] = 0.1;
+	state->att_quaternion[1] = 0.2;
+	state->att_quaternion[2] = 0.3;
+	state->att_quaternion[3] = 1.4;
 
 	/*Cleanup	*/
 	gsl_matrix_free(b_mtrx_p);
-	gsl_matrix_free(sun_vec_mtrx_p);
 	gsl_matrix_free(u_lu_mtrx_p);
 	gsl_matrix_free(v_mtrx_p);
 	gsl_matrix_free(v_lu_mtrx_p);
 	gsl_matrix_free(dcm_right_mtrx_p);
 	gsl_vector_free(s_vec_p);
 	gsl_vector_free(work_vec_p);
+	return;
+}
+
+/*
+ * Function: est_svd
+ * -----------------
+ *  Calculates an attitude estimate based on
+ *  the SVD solution to Whaba's problem.
+ *  
+ *  The body frame sun vector is calculated 
+ *  independantly by a Least Squares estimator
+ *
+ *  If the SC is in eclipse, the body rates are 
+ *  integrated in place of a sun vector
+ *
+ *  result: updated est_state 
+ */
+
+void est_svd(double* gyro_body_rates, double* mag_vec, double* sun_sens_volt, double* sun_sens_norm, double* sun_eci_vec, double* mag_eci_vec, struct est_state* state){
+
+	/*Initialize Matrices*/
+	gsl_matrix* sun_vec_mtrx_p = gsl_matrix_alloc(3,1);
+
+	/*Vector to Matrix Conversion*/
+	gsl_matrix_view mag_vec_mtrx = gsl_matrix_view_array(mag_vec,3,1);
+	gsl_matrix_view sun_eci_vec_mtrx = gsl_matrix_view_array(sun_eci_vec,3,1);
+	gsl_matrix_view mag_eci_vec_mtrx = gsl_matrix_view_array(mag_eci_vec,3,1);
+	gsl_matrix_view sun_sens_volt_mtrx = gsl_matrix_view_array(sun_sens_volt,SS_COUNT,1);
+	gsl_matrix_view sun_sens_norm_mtrx = gsl_matrix_view_array(sun_sens_norm,SS_COUNT,3);
+
+	/*Calc Sun Vector*/ 
+	int sv_ret;
+	sv_ret = est_sun_vec_ls(&sun_sens_volt_mtrx.matrix, &sun_sens_norm_mtrx.matrix, sun_vec_mtrx_p);
+	printf("Sun Vector:\n");
+	gsl_matrix_fprintf(stdout, sun_vec_mtrx_p,"%.2f");
+
+	/*Check if in Eclipse*/
+	if (sv_ret) { 
+        //Eclipse
+	 	printf("ECLIPSE: %d\n",sv_ret);
+		//Integrate Rate Gyros for 2nd Vector
+		//Calculate Estimate
+		est_svd_2v(&mag_eci_vec_mtrx.matrix,&sun_eci_vec_mtrx.matrix,&mag_vec_mtrx.matrix,sun_vec_mtrx_p,state);
+	} else {
+	//Sun
+	 	printf("DAY: %d\n",sv_ret);
+		//Calculate Estimate
+		est_svd_2v(&mag_eci_vec_mtrx.matrix,&sun_eci_vec_mtrx.matrix,&mag_vec_mtrx.matrix,sun_vec_mtrx_p,state);
+	}
+
+	/*Assign output Body Rates -- TEMP*/
+	state->sat_body_rates[0] = gyro_body_rates[0];
+	state->sat_body_rates[1] = gyro_body_rates[1];
+	state->sat_body_rates[2] = gyro_body_rates[2];
+
+	/*Cleanup	*/
+	gsl_matrix_free(sun_vec_mtrx_p);
 	return;
 }
 
--- test_est.c
+++ test_est.c
@@ -5,7 +5,7 @@ int main(void) {
 	//Input
 	double gyro_body_rates[] = {.01,.02,.03}; //DPS
 	double mag_vec[] = {.1, 1, 10}; //T
-	double sun_sens_volt[] = {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //V
+	double sun_sens_volt[] = {0.02, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}; //V
 	double mag_eci_vec[] = {1, 2, 3}; //Normalized Cartesian
 	double sun_eci_vec[] = {4, 5, 6}; //Normalized Cartesian
 	double sun_sens_norm[] = {1,0,0, 0,1,0, 0,0,1,
@@ -13,13 +13,11 @@ int main(void) {
 				  1,0,0, 0,1,0, 0,0,1,
 				  1,0,0, 0,1,0, 0,0,1,
 				  1,0,0, 0,1,0, 0,0,1};
-
 	//Output
-	double att_quaternion[4]; //Quaternion
-	double sat_body_rates[3]; //DPS 	
+	struct est_state state;
 
-	est_svd(gyro_body_rates, mag_vec, sun_sens_volt, sun_sens_norm, sun_eci_vec, mag_eci_vec, att_quaternion, sat_body_rates);
+	est_svd(gyro_body_rates, mag_vec, sun_sens_volt, sun_sens_norm, sun_eci_vec, mag_eci_vec, &state);
 
-	printf("Attitude: [%.2f, %.2f, %.2f, %.2f]   Body Rates: [%.2f, %.2f, %.2f]\n", att_quaternion[0],att_quaternion[1],att_quaternion[2],att_quaternion[3],sat_body_rates[0],sat_body_rates[1],sat_body_rates[2]);
+	printf("Attitude: [%.2f, %.2f, %.2f, %.2f]   Body Rates: [%.2f, %.2f, %.2f]\n", state.att_quaternion[0], state.att_quaternion[1], state.att_quaternion[2], state.att_quaternion[3], state.sat_body_rates[0], state.sat_body_rates[1], state.sat_body_rates[2]);
 	return 0;
 }
