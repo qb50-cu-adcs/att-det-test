182150b32e7ca24736503f418b2bcb8b075c42ee
nira6794 <nira6794@vaio_rhel_6.(none)>
2014-10-24 22:17:31 -0600

    det, mult, norm

--- include/mtx.h
+++ include/mtx.h
@@ -1,4 +1,4 @@
-#define MAX_ROWS 4
+#define MAX_ROWS 8
 #define MAX_COLS 4
 
 struct mtx_matrix{
@@ -14,3 +14,7 @@ void mtx_set(int row, int col, struct mtx_matrix* mtx_a, float data);
 void mtx_print(struct mtx_matrix* mtx_a);
 int mtx_sum(struct mtx_matrix* mtx_a, struct mtx_matrix* mtx_b, struct mtx_matrix* mtx_out);
 int mtx_scale(struct mtx_matrix* mtx_a, float scaler, struct mtx_matrix* mtx_out);
+int mtx_mult(struct mtx_matrix* mtx_a, struct mtx_matrix* mtx_b, struct mtx_matrix* mtx_out);
+int mtx_trans(struct mtx_matrix* mtx_a, struct mtx_matrix* mtx_out);
+float mtx_det(struct mtx_matrix* mtx_a);
+float mtx_norm(struct mtx_matrix* mtx_a);
