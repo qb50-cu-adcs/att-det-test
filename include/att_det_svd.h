3e161ceb418b7a66c8e4c2a1694e2b638c39e29c
nira6794 <nira6794@vaio_rhel_6.(none)>
2014-10-26 12:01:53 -0600

    Trace, Max

---
+++ include/att_det_svd.h
@@ -0,0 +1,10 @@
+#define SS_V_CUTOFF	.01
+#define SS_COUNT	15
+#define UPDATE_RATE	2.0
+struct est_state{
+	double att_quaternion[4];
+	double att_err[9];
+	double sat_body_rates[3];
+	double gyro_bias[3];
+}; 
+void est_svd(double* gyro_body_rates, double* mag_vec, double* sun_sens_volt, double* sun_sens_norm, double* sun_eci_vec, double* mag_eci_vec, struct est_state* state);
--- include/mtx.h
+++ include/mtx.h
@@ -10,7 +10,7 @@ struct mtx_matrix{
 void mtx_create(int rows, int cols, float *data, struct mtx_matrix* mtx_out);
 void mtx_create_ones(int rows, int cols, struct mtx_matrix* mtx_out);
 float mtx_get(int row, int col, struct mtx_matrix* mtx_a);
-void mtx_set(int row, int col, struct mtx_matrix* mtx_a, float data);
+void mtx_set(int row, int col, struct mtx_matrix* mtx_out, float data);
 void mtx_print(struct mtx_matrix* mtx_a);
 int mtx_sum(struct mtx_matrix* mtx_a, struct mtx_matrix* mtx_b, struct mtx_matrix* mtx_out);
 int mtx_scale(struct mtx_matrix* mtx_a, float scaler, struct mtx_matrix* mtx_out);
@@ -18,3 +18,6 @@ int mtx_mult(struct mtx_matrix* mtx_a, struct mtx_matrix* mtx_b, struct mtx_matr
 int mtx_trans(struct mtx_matrix* mtx_a, struct mtx_matrix* mtx_out);
 float mtx_det(struct mtx_matrix* mtx_a);
 float mtx_norm(struct mtx_matrix* mtx_a);
+int mtx_inv(struct mtx_matrix* mtx_a, struct mtx_matrix* mtx_out);
+float mtx_max(struct mtx_matrix* mtx_a, int* loc_row, int* loc_col);
+float mtx_trace(struct mtx_matrix* mtx_a);
